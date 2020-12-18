c8664b56c8889b780f18888ff5d602717380d015
Diana Quintal <dquinta@wested.org>
2020-12-17 16:11:18 -0800

    fix bug that prevented grouped multi-selection questions from getting created with the correct set of answers

--- lib/surveyor_gui/models/question_methods.rb
+++ lib/surveyor_gui/models/question_methods.rb
@@ -47,8 +47,9 @@ module SurveyorGui
             #don't set answer.text if question_type = number.  In this case, text should get set by the prefix and suffix setters.
             #note: Surveyor uses the answer.text field to store prefix and suffix for numbers.
             #if not a number question, go ahead and set the text attribute as normal.
-            if @question_type_id!="number" && !ans.empty? && ans["0"]
-              ans["0"].merge!( {"original_choice"=>ans["0"]["text"]})
+            if @question_type_id!="number" && !ans.empty?
+              first_answer_key = ans.keys.first
+              ans[first_answer_key].merge!( {"original_choice"=>ans[first_answer_key]["text"]})
               assign_nested_attributes_for_collection_association(:answers, ans)
             end
           end
