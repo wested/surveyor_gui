c19002480771fcfaf0103dc767971cc824f9c616
Diana Quintal <dquinta@wested.org>
2021-08-05 23:42:48 -0700

    when cloning skip the question building callback

--- lib/surveyor_gui/surveyforms_controller_methods.rb
+++ lib/surveyor_gui/surveyforms_controller_methods.rb
@@ -222,6 +222,10 @@ module SurveyorGui
     def clone_survey
       @title = "Clone Survey"
       @surveyform = SurveyCloneFactory.new(params[:id], true).clone
+
+      # don't need to run this callback since all data in the survey is already all set up since it was cloned from an existing one
+      Question.skip_callback(:save, :after, :build_complex_questions)
+
       if @surveyform.save
         flash[:notice] = "Successfully created survey, questionnaire, or form."
         redirect_to edit_surveyform_path(@surveyform)
@@ -229,6 +233,8 @@ module SurveyorGui
         flash[:error] = "Could not clone the survey, questionnaire, or form."
         render :action => 'new'
       end
+
+      Question.set_callback(:save, :after, :build_complex_questions)
     end
 
     private
